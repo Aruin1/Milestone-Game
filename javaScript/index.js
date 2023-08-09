fe08b3ae7b81df96b01e8517fdf3e288bc5dfe0e
Aruin1 <tristan.j.glock@gmail.com>
2023-08-08 23:56:37 -0400

    fixed mousemove function to work only in playarea

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,12 +1,13 @@
 // main function here
 let mousePos = {x: null, y: null}
 
-window.addEventListener('mousemove', (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {let playArea = document.getElementById('play-area')
+if(playArea) 
+{playArea.addEventListener('mousemove', (event) => {
   mousePos = { x: event.clientX, y: event.clientY };
 
   console.log(mousePos.x, mousePos.y)
-});
-
+})}})
 
 
 
